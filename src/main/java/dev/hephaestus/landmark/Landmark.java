6537b519cccd84ff215c748aaa9be50b40bd6bac
Haven <haven@hephaestus.dev>
2020-06-11 15:42:34 -0700

    Initial Commit

---
+++ src/main/java/dev/hephaestus/landmark/Landmark.java
@@ -0,0 +1,10 @@
+package dev.hephaestus.landmark;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Landmark implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
